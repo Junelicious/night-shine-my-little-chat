87839319bbed0e2e6efe4c3d4542b560fefa07f3
Junelicious <junelicious@outlook.com>
2016-12-19 23:21:48 +0300

    Update nightshine.user.js

--- nightshine.user.js
+++ nightshine.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Night Mode redesign
+// @name         NightShine skin for My Little Chat
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
