d8600ed8545ecba0ed25b8cdbb5d79483e15b1b2
Junelicious <junelicious@outlook.com>
2016-12-21 15:15:09 +0300

    Revert "testing the push hooks"
    This reverts commit 18a54e06df12aaee219e903f616fb517e199b6a3.
    
    # Conflicts:
    #	nightshine.user.js

--- nightshine.user.js
+++ nightshine.user.js
@@ -1,8 +1,12 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @name         NightShine skin for My Little Chat
 // @namespace    http://tampermonkey.net/
+<<<<<<< HEAD
 // @version      0.151
 // @updateURL    https://openuserjs.org/meta/Junelicious/NightShine_skin_for_My_Little_Chat.meta.js
+=======
+// @version      0.15
+>>>>>>> parent of 18a54e0... testing the push hooks
 // @description  Изменяет вид ночного режима MyLittleChat.ru, автор не несет никакой ответственности за нанесенные повреждения вашей психике, компутеру, коту.. и другим предметам.
 // @author       June Aurelious
 // @match https://mylittlechat.ru/%D0%9A%D0%BE%D0%BC%D0%BD%D0%B0%D1%82%D0%B8
